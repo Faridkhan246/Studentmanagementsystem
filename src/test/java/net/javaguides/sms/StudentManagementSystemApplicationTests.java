0538d7c283e8b37ce62a6751cee4d2780b61831f
Faridkhan246 <faridkhan246246@gmail.com>
2024-05-23 09:31:29 +0530

    first commit

---
+++ src/test/java/net/javaguides/sms/StudentManagementSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package net.javaguides.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class StudentManagementSystemApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+
+}
